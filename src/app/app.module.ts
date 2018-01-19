741e24f0d7f2bec53ca0b3cd5a1cf7efc1686fff
sam <samkissee@gmail.com>
2018-01-19 12:42:55 -0800

    add pipe via cli, next step create function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppComponent } from './app.component';
 import { DirectoryComponent } from './directory/directory.component';
 import { AddAnimalComponent } from './add-animal/add-animal.component';
 import { EditAnimalComponent } from './edit-animal/edit-animal.component';
+import { AgeSortPipe } from './age-sort.pipe';
 
 
 
@@ -18,7 +19,8 @@ import { EditAnimalComponent } from './edit-animal/edit-animal.component';
     AppComponent,
     DirectoryComponent,
     AddAnimalComponent,
-    EditAnimalComponent
+    EditAnimalComponent,
+    AgeSortPipe
   ],
   imports: [
     BrowserModule,
